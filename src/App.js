b168fe0d132c53e3c4acf679f5b063830db15c99
d-car <danielscarstensen@gmail.com>
2018-10-08 23:38:18 -0500

    images and kongs.json list appending

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import KongCard from "./components/KongCard/KongCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import kongs from "./kongs.json";
+import "./App.css";
 
 class App extends Component {
+  // Setting this.state.kongs to the kongs json array
+  state = {
+    kongs
+  };
+
+  removeKong = id => {
+    // Filter this.state.kongs for kongs with an id not equal to the id being removed
+    const kongs = this.state.kongs.filter(kong => kong.id !== id);
+    // Set this.state.kongs equal to the new kongs array
+    this.setState({ kongs });
+  };
+
+  // Map over this.state.kongs and render a KongCard component for each kong object
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Wrapper>
+        <Title>kongs List</Title>
+        {this.state.kongs.map(kong => (
+          <KongCard
+            removekong={this.removekong}
+            id={kong.id}
+            key={kong.id}
+            name={kong.name}
+            image={kong.image}
+          />
+        ))}
+      </Wrapper>
     );
   }
 }
 
-export default App;
+export default App
---
+++ src/components/KongCard/KongCard.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./KongCard.css";
+
+const KongCard = props => (
+  <div className="card">
+    <div className="img-container">
+      <img alt={props.name} src={props.image} />
+    </div>
+    <div className="content">
+      <ul>
+        <li>
+          <strong>Name:</strong> {props.name}
+        </li>
+      </ul>
+    </div>
+    <span onClick={() => props.removeFriend(props.id)} className="remove">
+      𝘅
+    </span>
+  </div>
+);
+
+export default KongCard;
