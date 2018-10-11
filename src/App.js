92673982ada224810e0652d65b270918218f31fb
unknown <danie@Dan-Laptop>
2018-10-11 15:14:12 -0500

    changed render function to have cards in array

--- src/App.js
+++ src/App.js
@@ -11,29 +11,50 @@ class App extends Component {
     kongs
   };
 
-  removeKong = id => {
-    // Filter this.state.kongs for kongs with an id not equal to the id being removed
-    const kongs = this.state.kongs.filter(kong => kong.id !== id);
-    // Set this.state.kongs equal to the new kongs array
-    this.setState({ kongs });
-  };
+  // removeKong = id => {
+  //   // Filter this.state.kongs for kongs with an id not equal to the id being removed
+  //   const kongs = this.state.kongs.filter(kong => kong.id !== id);
+  //   // Set this.state.kongs equal to the new kongs array
+  //   this.setState({ kongs });
+  // };
 
   // Map over this.state.kongs and render a KongCard component for each kong object
-  render() {
-    return (
-      <Wrapper>
-        <Title>kongs List</Title>
-        {this.state.kongs.map(kong => (
-          <KongCard
-            removekong={this.removekong}
-            id={kong.id}
-            key={kong.id}
-            name={kong.name}
-            image={kong.image}
-          />
-        ))}
-      </Wrapper>
-    );
+  render = (array) => {
+    return this.state.kongs.map(kong => ( 
+      <section className='col s4 m3 l3' key={kong.id} id={kong.id}>
+        <KongCard
+          name={kong.name} 
+          image={kong.image} 
+          reArrangeKongs={() => {this.reArrangeKongs(this.state.Kongs)}}
+          clickedCharacter={() => {this.clickedCharacter(kong.id)}}/>
+      </section>
+    )
+  )
+
+    
+
+
+
+
+
+
+
+
+
+
+      // <Wrapper>
+      //   <Title>kongs List</Title>
+      //   {this.state.kongs.map(kong => (
+      //     <KongCard
+      //       removekong={this.removekong}
+      //       id={kong.id}
+      //       key={kong.id}
+      //       name={kong.name}
+      //       image={kong.image}
+      //     />
+      //   ))}
+      // </Wrapper>
+    // );
   }
 }
 
